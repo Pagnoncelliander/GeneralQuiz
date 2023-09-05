2b9ff1135e8c537b30e00f70d26dc00e12e41cfb
Pagnoncelliander <134398235+Pagnoncelliander@users.noreply.github.com>
2023-09-05 19:51:20 +0100

    Update script.js
    created   const to display to show the correct answer after click at the answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,20 @@
 const questions = [
     {
         question: "What is the capital of France?",
-        options: [{ text: "London" }, { text: "Berlin" }, { text: "Madrid" }, { text: "Paris" }],
+        options: [{text:"London"}, {text:"Berlin"}, {text:"Madrid"}, {text:"Paris"}],
         correctIndex: 3
     },
     {
-        question: "Which planet is known as the 'Red Planet'?",
-        options: [{ text: "Mars" }, { text: "Venus" }, { text: "Jupiter" }, { text: "Saturn" }],
-        correctIndex: 0
-    },
-    {
-        question: "On what continent would you find the world’s largest desert?",
-        options: [{ text: "Asia" }, { text: "Antarctica" }, { text: "America" }, { text: "Europe" }],
+        question: "which is larget animal in the world?",
+        options: [{text:"Shark"}, {text:"Blue whale"}, {text:"Elephant"}, {text:"Girafe"}],
         correctIndex: 1
     },
     {
         question: "Where is Disney's European theme park located?",
-        options: [{ text: "United State, Orlando" }, { text: "Brazil, Rio de Janeiro" }, { text: "Paris, France" }, { text: "Great Britain, London" }],
-        correctIndex: 2
-    },
-    {
-        question: "December 26th is known by what names in Ireland?",
-        options: [{ text: "st patrick's day" }, { text: "Ireland independence day" }, { text: "christmas day" }, { text: "Saint Stephen's Day" }],
+        options: [{text:"London, England"}, {text:"Nice, France"}, {text:"Milan, Italy"}, {text:"Paris, France"}],
         correctIndex: 3
-    }
+    },
+  
 ];
 
 const questionElement = document.getElementById("question");
@@ -33,45 +24,63 @@ const resultElement = document.getElementById("result");
 const tryAgain = document.getElementById("refresh");
 const NewGame = document.getElementById("new-game");
 
+const correctAnswerElement = document.getElementById("correct-answer");
+
 let currentQuestion = 0;
 let score = 0;
 
 
 /** created a function "if" to display the question html, click and display botton */
 function displayQuestion() {
-    if (currentQuestion < questions.length) { /*se currentQuestion = 0 for menor que questionse questions nela*/
-        const question2 = questions[currentQuestion];/* na variavel question2 foi colocado quesitoins"questions" dentro do currentequestion */
-        questionElement.textContent = question2.question; /* questionElement recebe o texto e atribui question2 e quetion*/
+    if (currentQuestion < questions.length) { 
+        const question2 = questions[currentQuestion];
+        questionElement.textContent = question2.question; 
 
         optionsElement.innerHTML = "";
         question2.options.forEach((option, correctIndex) => {
             const button = document.createElement("button");
             button.innerHTML = option.text;
             button.addEventListener("click", () => checkAnswer(correctIndex));
+           
+            button.classList.add("button");
+
             optionsElement.appendChild(button);
+            
+            correctAnswerElement.style.display = "none";
         });
-        nextButton.style.display = "block";
-        tryAgain.style.display = "none";
-        NewGame.style.display = "none";
-    } else {
-        showResult();
-    }
+            nextButton.style.display = "block"; 
+            tryAgain.style.display = "none";
+            NewGame.style.display = "none";
+        } else {
+            showResult();
+        }
 }
 
-/**this function will check each question and display the correct question */
-function checkAnswer(selectedIndex) {
+
+    /**this function will check each question and display the correct question */
+    function checkAnswer(selectedIndex) {
     const question = questions[currentQuestion];
     if (selectedIndex === question.correctIndex) {
         score++;
     }
 
+    
+    /**created to show the correct answer for each question */
+    
+    const correctAnswerText = question.options[question.correctIndex].text;
+    correctAnswerElement.textContent = `The correct Answer: ${correctAnswerText}`;
+    correctAnswerElement.style.display = "block"; 
+
     currentQuestion++;
     nextButton.style.display = "block";
-
+    
     // Disable buttons after an answer is selected
     const buttons = optionsElement.querySelectorAll("button");
     buttons.forEach(button => button.disabled = true);
+
 }
+
+
 
 
 //**this function will compare the score and currentquestion and advise if score is goods or should try again */
@@ -84,24 +93,24 @@ function showResult() {
     /**getting element from inner1(windown) */
     const NameLevel = window.location.search;
     const searchParams = new URLSearchParams(NameLevel);
-    const param1 = searchParams.get('first_name');
-
-    if (score <= 2) {
+    const param1  = searchParams.get('first_name');
+    
+    if(score <= 2){
         Homepage();
-        resultElement.textContent = ` ${param1} Your score: ${score} out of ${questions.length} you can do better next time`;
+        resultElement.innerHTML = ` ${param1} Your score: ${score} out of ${questions.length}<br> Don't focus on the loss, focus on the next win.<br> Please try Again!!`;
 
-    } else if (score === currentQuestion) {
-        resultElement.textContent = ` ${param1} Your score: ${score} out of ${questions.length} Congratulation!!`;
+    }else if (score === currentQuestion){
+        resultElement.innerHTML = ` ${param1} Your score: ${score} out of ${questions.length}<br Congratulation, You Won!!`;
 
-    } else if (score <= 4) {
-        resultElement.textContent = ` ${param1} Your score: ${score} out of ${questions.length} Almost there, try again! `;
+    }else if(score <= 4){
+        resultElement.innerHTML = ` ${param1} Your score: ${score} out of ${questions.length} <br> Almost there, Try Again! `;
     };
     NewGame.style.display = "block";
-
+    correctAnswerElement.style.display = "none";
 }
 
 
-/**button to start quiz again */
+    /**button to start quiz again */
 nextButton.addEventListener("click", () => {
     const buttons = optionsElement.querySelectorAll("button");
     buttons.forEach(button => button.disabled = false);
@@ -110,14 +119,16 @@ nextButton.addEventListener("click", () => {
 });
 
 
+
 /**button start again */
-function Homepage() {
-    tryAgain.style.display = "block";
-    tryAgain.addEventListener("click", () => {
-        location.reload();
-    });
+function Homepage(){
+    tryAgain.style.display = "block"; 
+    tryAgain.addEventListener("click", () =>{
+    location.reload();
+});
 };
 
 Homepage();
 
 displayQuestion();
+
